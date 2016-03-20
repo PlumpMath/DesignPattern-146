ce91fd326de286a405188a07a96a35cd1fd22b70
kun368 <kun368@126.com>
2016-03-20 16:33:49 +0800

    更新观察者模式实验报告

--- src/test/java/com/zzkun/oberver/MyTest.java
+++ src/test/java/com/zzkun/oberver/MyTest.java
@@ -7,10 +7,8 @@ import org.junit.Test;
  * Created by kun on 2016/3/16.
  */
 public class MyTest {
-
     @Test
     public void test() {
-
         Weather weather = new Weather(23, 100, 101);
         Hotel hotel = new Hotel("青岛皇家旅馆", "良好", "17854258245");
         Guide guide = new Guide("小王");
@@ -20,6 +18,5 @@ public class MyTest {
         tourist.subscribe(weather).subscribe(hotel);
 
         weather.setTemperature(30);
-
     }
 }
